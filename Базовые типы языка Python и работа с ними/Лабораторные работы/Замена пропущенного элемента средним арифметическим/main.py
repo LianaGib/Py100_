4209ec3f909ad7f0882fdf84c8821fe6749a76d4
Liana Lesiva <gibaevaliana@mail.ru>
2023-10-24 22:17:29 +0300

    init

--- Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
@@ -1,8 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-len_ = (len(numbers))
-numbers.remove(None)
-average_value = sum(numbers)/len_
-for num in numbers:
-    if (num == None):
-         num = average_value
+average_value = sum([x for x in numbers if x != None])/len(numbers)
+for x in range(len(numbers)):
+    if numbers[x] == None:
+        numbers[x] = average_value
 print("Измененный список:", numbers)
