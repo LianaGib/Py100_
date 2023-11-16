b1aab3cfa70762835469d5f46e9f4e845a454eb7
LianaGib <143552368+LianaGib@users.noreply.github.com>
2023-11-16 22:44:10 +0300

    init

--- Функции и методы/Самостоятельная работа/task5/main.py
+++ Функции и методы/Самостоятельная работа/task5/main.py
@@ -1,6 +1,9 @@
 # TODO реализовать функцию
-def get_sentences_list():
-    ...
+def get_sentences_list(text_):
+    text_1 = text_.split(".")
+
+    return text_1
+
 
 
 print(get_sentences_list("Здесь много разных слов. Возможно и много повторений..."))
